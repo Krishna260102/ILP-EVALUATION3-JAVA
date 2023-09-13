c0262a75e5defeceeee417d4f4659e9c700fcff5
Krishna260102 <101826520+Krishna260102@users.noreply.github.com>
2023-09-13 20:42:37 +0530

    Delete YONO-APP directory

--- YONO-APP/src/main/java/com/nissan/app/exceptions/EntityNotFoundException.java
+++
@@ -1,7 +0,0 @@
-package com.nissan.app.exceptions;
-
-public class EntityNotFoundException extends RuntimeException {
-    public EntityNotFoundException(String message) {
-        super(message);
-    }
-}
--- YONO-APP/src/main/java/com/nissan/app/exceptions/InsufficientFundsException.java
+++
@@ -1,8 +0,0 @@
-package com.nissan.app.exceptions;
-
-
-public class InsufficientFundsException extends RuntimeException {
-    public InsufficientFundsException(String message) {
-        super(message);
-    }
-}
